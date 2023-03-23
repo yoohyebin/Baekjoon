2b2eadc9778ce0e07dc3505acae178b57437dd28
Hyebin (Helia) <hyebin218@naver.com>
2023-03-23 22:58:58 +0900

    Update 2346.swift

--- 코딩테스트_대비_문제집(with_Baekjoon)/Data_Structure/2346.swift
+++ 코딩테스트_대비_문제집(with_Baekjoon)/Data_Structure/2346.swift
@@ -1,15 +1,16 @@
-let n = Int(readLine()!)!
-var arr = readLine()!.split(separator: " ").enumerated().map{($0.offset, Int(String($0.element))!)}
-var result = ""
-var index = 0
+//8ms
+let _ = Int(readLine()!)
+var balloons = readLine()!.split(separator: " ").enumerated().map{ ($0+1,Int(String($1))!) }
+var index = 0, result = ""
 
 while true {
-    var temp = arr[index].1
-    result += "\(arr.remove(at: index).0+1) "
-    index += (temp > 0 ? temp-1 : temp)
-    
-    if arr.isEmpty {break}
-    index = (index >= 0) ? index : arr.count + index % arr.count
-    index %= arr.count
+    var current = balloons.remove(at: index)
+    result += "\(current.0) "
+    if balloons.count == 0 { break }
+
+    index += current.1 > 0 ? (current.1-1) : current.1
+    index = (index >= 0) ? index : balloons.count + index % balloons.count
+    index %= balloons.count
 }
+
 print(result)
