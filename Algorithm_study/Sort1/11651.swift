73056da9233aa572c97d44de4e96bf9e554f28ab
Hyebin (Helia) <hyebin218@naver.com>
2023-04-05 20:23:00 +0900

    Create 11651.swift

---
+++ Algorithm_study/Sort1/11651.swift
@@ -0,0 +1,11 @@
+//252ms
+let N = Int(readLine()!)!
+var xy = [[Int]]()
+
+for _ in 0..<N {
+    xy.append(readLine()!.split(separator: " ").map{Int(String($0))!})
+}
+
+for i in xy.sorted(by: {$0[1] == $1[1] ? $0[0] < $1[0] : $0[1] < $1[1]}) {
+    print("\(i[0]) \(i[1])")
+}
