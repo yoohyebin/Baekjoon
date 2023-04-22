0157a96b191f84aa5f6a5c94fc91bd742042684e
Hyebin (Helia) <hyebin218@naver.com>
2023-04-22 23:59:20 +0900

    Update 19532.swift

--- 코딩테스트_대비_문제집(with_Baekjoon)/19532.swift
+++ 코딩테스트_대비_문제집(with_Baekjoon)/19532.swift
@@ -10,3 +10,19 @@ for i in -999..<1000 {
         }
     }
 }
+
+//8ms
+let input = readLine()!.split(separator: " ").map{Int(String($0))!}
+let a = input[0], b = input[1], c = input[2], d = input[3], e = input[4], f = input[5]
+var x: Int, y: Int
+
+if a == 0{
+    y = c / b
+    x = (f - e*y)/d
+}
+else{
+    y = (c*d - f*a) / (b*d - e*a)
+    x = (c - b*y)/a
+}
+
+print("\(x) \(y)")
